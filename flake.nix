413211d8a0d4718c4ec378290f705a2bbe26bafa
drzzln <drzzln@protonmail.com>
2026-03-15 19:56:00 -0400

    add nix flake with substrate integration

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Akeyless support bundle collector - gathers diagnostic information from Akeyless deployments";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [ shellcheck bash ];
+      };
+    });
+}
